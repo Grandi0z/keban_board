e793fdc8ece52e6a3e435689d27410534845f475
unknown <josephlkausi@gmail.com>
2023-05-18 17:15:57 +0200

    Fix linters errors

--- Js/kaban.js
+++ Js/kaban.js
@@ -1,7 +1,3 @@
-import KabanApi from "./api/kabanApi.js";
-import KabanView from "./view/kabanView.js";
-//console.log(KabanApi.getItems(2))
-//KabanApi.insertItems(2,"just test")
-//KabanApi.updateItems(42228,{columnId:1, position:2,content:"New 2 content"})
-//KabanApi.deleteItems(15271)
-new KabanView(document.querySelector(".kaban")) 
+import KabanView from './view/kabanView.js';
+
+KabanView(document.querySelector('.kaban'));
