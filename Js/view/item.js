7901e70137563d9fcd866b793106a658614d0b57
unknown <josephlkausi@gmail.com>
2023-05-18 17:21:21 +0200

    Fix eslint errors

--- Js/view/item.js
+++ Js/view/item.js
@@ -21,7 +21,8 @@ export default class Item {
 
     this.elements.input.addEventListener('blur', onBlur);
     this.elements.root.addEventListener('dblclick', () => {
-      const check = true //window.confirm('Do you really want to confirm this ?');
+      const check = true;
+      //window.confirm('Do you really want to confirm this ?');
       if (check) {
         KabanApi.deleteItems(id);
         this.elements.input.removeEventListener('blur', onBlur);
